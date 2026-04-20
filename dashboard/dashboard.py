25d384607b752220c4849f8cf2acbffedab66b81
Chairun Nisaq <145679533+broomrun@users.noreply.github.com>
2026-04-20 23:27:44 +0700

    bismillah

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,7 +7,7 @@ import matplotlib.ticker as ticker
 st.set_page_config(layout="wide")
 
 # Load Data
-df = pd.read_csv("main_data.csv")
+df = pd.read_csv("dashboard/main_data.csv")
 df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
 
 
